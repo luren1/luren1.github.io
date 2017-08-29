e7ea3bafc79d12f5bcd5c4bff11f73e77f63564b
xxk <847220475@qq.com>
2017-08-29 22:13:24 +0800

    add 2048

---
+++ 2048/js/functions.js
@@ -0,0 +1,44 @@
+//随机颜色
+var getRandomColor = function(){
+  return  '#' +    
+    (function(color){    
+    return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])    
+      && (color.length == 6) ?  color : arguments.callee(color);    
+  })('');
+}
+
+//当前时间
+function CurrentTime(){ 
+        var now = new Date();
+        
+        var year = now.getFullYear();       //年
+        var month = now.getMonth() + 1;     //月
+        var day = now.getDate();            //日
+        
+        var hh = now.getHours();            //时
+        var mm = now.getMinutes();          //分
+        var ss = now.getSeconds();           //秒
+        
+        var clock = year + "-";
+        
+        if(month < 10)
+            clock += "0";
+        
+        clock += month + "-";
+        
+        if(day < 10)
+            clock += "0";
+            
+        clock += day + " ";
+        
+        if(hh < 10)
+            clock += "0";
+            
+        clock += hh + ":";
+        if (mm < 10) clock += '0'; 
+        clock += mm + ":"; 
+         
+        if (ss < 10) clock += '0'; 
+        clock += ss; 
+        return(clock); 
+}
